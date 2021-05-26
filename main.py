1e3529d8ed576c914fcd0d17537b0b725af57bff
zzzBECK <alexandrebeck2003@gmail.com>
2021-05-26 19:44:46 -0300

    funcao score

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from graphics import *
 from ui import *
+from score import *
 
 win = GraphWin("Bolinha Game", 800, 600)
 playing = True
+
+
+pts = 0
+pontos=Text(Point(400, 575), "Pontos: " + str(pts))
+pontos.setSize(14)
+pontos.draw(win)
+
 
 while playing:
     key = win.checkKey()
